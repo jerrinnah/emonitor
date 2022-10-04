dc738d1d367b91c299d5531f87c0fca06d15c983
NEJ <devjerrynnah@gmail.com>
2022-10-04 22:03:15 +0100

    Styled Navigation Btns

--- src/screens/Navigation.jsx/Navigation.jsx
+++ src/screens/Navigation.jsx/Navigation.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import FeedScreen from '../FeedScreen';
 import AddPost from '../AddPost';
 import Explore from '../Explore';
@@ -8,29 +8,48 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Entypo } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
-  
+
 const Navigation = () => (
-    <Tab.Navigator screenOptions={{
-      headerShown:false,
-      // tabBarActiveBackgroundColor: "blue",
-      // tabBarActiveTintColor:'white',
-    }}>
-  
-      <Tab.Screen name='Home' component={FeedScreen} options={{
-        tabBarIcon: ()=> <Entypo name='home' size={30}/>
-      }} />
-      <Tab.Screen name='Add Post' component={AddPost} options={{
-        tabBarIcon:()=> <Entypo name='circle-with-plus' size={30}/>
-      }} />
-      <Tab.Screen name='Explore' component={Explore} options={{
-        tabBarIcon:()=> <Entypo name='add-to-list' size={30}/>
-      }} />
-      <Tab.Screen name='Profile' component={Profile} options={{
-        tabBarIcon:()=> <Entypo name='user' size={30}/>
-      }} />
-    </Tab.Navigator>
-  )
+  <Tab.Navigator
+    screenOptions={{
+      headerShown: false,
+      tabBarActiveBackgroundColor: 'transparent',
+      tabBarActiveTintColor: 'black',
+      tabBarInactiveBackgroundColor: 'white',
+      tabBarInactiveTintColor: 'lightgrey',
+    }}
+  >
+    <Tab.Screen
+      name="Home"
+      component={FeedScreen}
+      options={{
+          tabBarIcon: ({ size, color }) => <Entypo name="home" size={size} color={color} />,
+      }}
+    />
+    <Tab.Screen
+      name="Add Post"
+      component={AddPost}
+      options={{
+        tabBarIcon: ({ size, color }) => <Entypo name="circle-with-plus" size={size} color={color} />,
+      }}
+    />
+    <Tab.Screen
+      name="Explore"
+      component={Explore}
+      options={{
+        tabBarIcon: ({ size, color }) => <Entypo name="add-to-list" size={size} color={color} />,
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={Profile}
+      options={{
+        tabBarIcon: ({ size, color }) => <Entypo name="user" size={size} color={color} />,
+      }}
+    />
+  </Tab.Navigator>
+);
 
-export default Navigation
+export default Navigation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
